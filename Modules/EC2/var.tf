91c82dc39549da1ffe0a3693222f7f3f9fd365a2
sabeermohamed.in <94451890+srksabeer@users.noreply.github.com>
2024-06-08 12:09:59 +0530

    Create var.tf

---
+++ Modules/EC2/var.tf
@@ -0,0 +1,29 @@
+# EC2
+
+variable "ami_id" {
+  default = "ami-0183d80552093ddaf"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+variable "prod-api_key_name" {
+  default = "Prod-API-Astrocure"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "public-subnet-1_id" {
+  type = string
+}
+
+variable "public-subnet-2_id" {
+  type = string
+}
