4871cb880b608870e592db3cb46b1de8aa4f5577
sabeermohamed.in <94451890+srksabeer@users.noreply.github.com>
2024-06-08 11:17:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+
+# Create Prod Astrovidhi Backend-S3.
+module "Backend-S3" {
+  source = "./Modules/Backend-S3"
+}
+
+# Create Prod Astrovidhi DYNAMODB..
+module "DYNAMODB" {
+  source = "./Modules/DYNAMODB"
+}
+
