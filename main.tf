0637fbd34fb1e761f85cb5b1c513511d8b10d982
sabeermohamed.in <94451890+srksabeer@users.noreply.github.com>
2024-06-08 11:42:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,21 @@ module "DYNAMODB" {
 */
 
 
+# Create Prod myproperty  VPC..
+module "VPC" {
+  source = "./Modules/VPC"
+  vpc_id = module.VPC.vpc_id
+  internet_gateway_id = module.VPC.vpc_id
+  public-subnet-1_id  = module.VPC.vpc_id
+  public-subnet-2_id  = module.VPC.vpc_id
+  public-subnet-3_id  = module.VPC.vpc_id
+  public-route-table-1_id = module.VPC.vpc_id
+  public-subnet-1-route-table-association_id = module.VPC.vpc_id
+  private-subnet-1_id  = module.VPC.vpc_id
+  private-subnet-2_id  = module.VPC.vpc_id
+  private-subnet-3_id  = module.VPC.vpc_id
+  private-route-table-1_id = module.VPC.vpc_id
+  private-subnet-1-route-table-association_id = module.VPC.vpc_id
+  }
+
+
