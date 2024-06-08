034de6756c5c2dfe29c67d2aa59b1eb598ae0f44
sabeermohamed.in <94451890+srksabeer@users.noreply.github.com>
2024-06-08 11:21:08 +0530

    Create backend-s3.tf

---
+++ Modules/Backend-S3/backend-s3.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "prod-Mypropertyqr-terraform-remote-state"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "terraform_state" {
+  bucket = "${aws_s3_bucket.terraform_state.id}"
+  policy =<<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "RequireEncryption",
+   "Statement": [
+    {
+      "Sid": "RequireEncryptedTransport",
+      "Effect": "Deny",
+      "Action": ["s3:*"],
+      "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      },
+      "Principal": "*"
+    },
+    {
+      "Sid": "RequireEncryptedStorage",
+      "Effect": "Deny",
+      "Action": ["s3:PutObject"],
+      "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
+      "Condition": {
+        "StringNotEquals": {
+          "s3:x-amz-server-side-encryption": "AES256"
+        }
+      },
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+}
